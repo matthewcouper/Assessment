7f04230a4bf7c6cfd8e31469c541bb9407036cf7
Matthew Couper <51890733+matthewcouper@users.noreply.github.com>
2019-06-20 14:30:58 +1200

    Add files via upload

---
+++ Assessment/Level1_Dodge/FrmDodge.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Level1_Dodge
+{
+    partial class FrmDodge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.TmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.TmrShip = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.LblScore = new System.Windows.Forms.Label();
+            this.LblLives = new System.Windows.Forms.Label();
+            this.TxtLives = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LblScore1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MnuStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.MnuStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.TmrAnim = new System.Windows.Forms.Timer(this.components);
+            this.TmrAnim1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.PnlGame.BackgroundImage = global::Level1_Dodge.Properties.Resources._2001_space_odyssey_800x450;
+            this.PnlGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PnlGame.Location = new System.Drawing.Point(12, 72);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(560, 390);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // TmrPlanet
+            // 
+            this.TmrPlanet.Tick += new System.EventHandler(this.TmrPlanet_Tick);
+            // 
+            // TmrShip
+            // 
+            this.TmrShip.Interval = 50;
+            this.TmrShip.Tick += new System.EventHandler(this.TmrShip_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Haettenschweiler", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(170, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "2001 A SPACE ARCADE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(194, 426);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(100, 20);
+            this.TxtName.TabIndex = 3;
+            this.TxtName.TextChanged += new System.EventHandler(this.TxtName_TextChanged);
+            // 
+            // LblScore
+            // 
+            this.LblScore.AutoSize = true;
+            this.LblScore.Location = new System.Drawing.Point(469, 242);
+            this.LblScore.Name = "LblScore";
+            this.LblScore.Size = new System.Drawing.Size(0, 13);
+            this.LblScore.TabIndex = 4;
+            // 
+            // LblLives
+            // 
+            this.LblLives.AutoSize = true;
+            this.LblLives.Location = new System.Drawing.Point(469, 277);
+            this.LblLives.Name = "LblLives";
+            this.LblLives.Size = new System.Drawing.Size(32, 13);
+            this.LblLives.TabIndex = 5;
+            this.LblLives.Text = "Lives";
+            this.LblLives.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // TxtLives
+            // 
+            this.TxtLives.Location = new System.Drawing.Point(341, 426);
+            this.TxtLives.Name = "TxtLives";
+            this.TxtLives.Size = new System.Drawing.Size(16, 20);
+            this.TxtLives.TabIndex = 6;
+            this.TxtLives.Text = "5";
+            this.TxtLives.TextChanged += new System.EventHandler(this.TxtLives_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(469, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Score";
+            // 
+            // LblScore1
+            // 
+            this.LblScore1.Location = new System.Drawing.Point(310, 426);
+            this.LblScore1.Name = "LblScore1";
+            this.LblScore1.Size = new System.Drawing.Size(16, 20);
+            this.LblScore1.TabIndex = 9;
+            this.LblScore1.Text = "0";
+            this.LblScore1.TextChanged += new System.EventHandler(this.LblScore1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MnuStart,
+            this.MnuStop});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MnuStart
+            // 
+            this.MnuStart.Name = "MnuStart";
+            this.MnuStart.Size = new System.Drawing.Size(43, 20);
+            this.MnuStart.Text = "Start";
+            this.MnuStart.Click += new System.EventHandler(this.MnuStart_Click);
+            // 
+            // MnuStop
+            // 
+            this.MnuStop.Name = "MnuStop";
+            this.MnuStop.Size = new System.Drawing.Size(43, 20);
+            this.MnuStop.Text = "Stop";
+            this.MnuStop.Click += new System.EventHandler(this.MnuStop_Click);
+            // 
+            // TmrAnim
+            // 
+            this.TmrAnim.Interval = 10;
+            this.TmrAnim.Tick += new System.EventHandler(this.TmrAnim_Tick);
+            // 
+            // TmrAnim1
+            // 
+            this.TmrAnim1.Tick += new System.EventHandler(this.TmrAnim1_Tick);
+            // 
+            // FrmDodge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.LblScore1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtLives);
+            this.Controls.Add(this.LblLives);
+            this.Controls.Add(this.LblScore);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PnlGame);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.Name = "FrmDodge";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dodge";
+            this.Load += new System.EventHandler(this.FrmDodge_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmDodge_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.FrmDodge_KeyUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Timer TmrPlanet;
+        private System.Windows.Forms.Timer TmrShip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Label LblScore;
+        private System.Windows.Forms.Label LblLives;
+        private System.Windows.Forms.TextBox TxtLives;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox LblScore1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MnuStart;
+        private System.Windows.Forms.ToolStripMenuItem MnuStop;
+        private System.Windows.Forms.Timer TmrAnim;
+        private System.Windows.Forms.Timer TmrAnim1;
+    }
+}
+
---
+++ Assessment/Level1_Dodge/FrmDodge.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Level1_Dodge
+{
+    public partial class FrmDodge : Form
+    {
+
+        Graphics g; // declare the graphics object
+        int x = 20, y = 20;// starting position of planet
+        Random speed = new Random();
+        int[] planetSpeed = new int[7];
+        int score = 0;
+        int lives = 5;
+        bool left, right, up, down;
+        // declare a rectangle to contain the spaceship and an area array to contain the planets
+        Rectangle areaSpaceship;
+        Rectangle[] area = new Rectangle[7];//area[0] to area[6]
+
+
+        private void TmrPlanet_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                area[i].Y += planetSpeed[i];
+                //if spaceship collides with any planet lose a life and move planet to the top of the panel
+                if (area[i].IntersectsWith(areaSpaceship))
+                {
+                    area[i].Y = 20;
+                    lives -= 1; // reduce lives by 1
+                                //display the number of lives on the form
+                    TxtLives.Text = lives.ToString();
+
+                    CheckLives();
+                }
+                if (area[i].Y > PnlGame.Height)
+                {
+                    area[i].Y = 20;
+                    score += 1; // add 1 to score
+                    LblScore1.Text = score.ToString();//display score on the form 
+                }
+
+            }
+            PnlGame.Invalidate();
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic, null, PnlGame, new object[] { true });
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+
+            //get the methods from the graphic's class to paint on the panel
+            g = e.Graphics;
+            //use the DrawImage method to draw the spaceship on the panel
+            g.DrawImage(alien1, areaSpaceship);
+            for (int i = 0; i <= 6; i++)
+            { 
+                g.DrawImage(planet1, area[i]);
+            }
+
+        }
+
+        int x2 = 190, y2 = 270; //starting position of spaceship
+                               //Load our two images from the bin\debug folder
+
+        Image alien1 = Image.FromFile(Application.StartupPath + @"\alien2.png");
+
+
+        private void FrmDodge_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = true; }
+            if (e.KeyData == Keys.Right) { right = true; }
+            if (e.KeyData == Keys.Up) { up = true; }
+            if (e.KeyData == Keys.Down) { down = true; }
+
+        }
+
+        private void FrmDodge_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) { left = false;}
+            if (e.KeyData == Keys.Right) { right = false;}
+            if (e.KeyData == Keys.Up) { up = false;}
+            if (e.KeyData == Keys.Down) { down = false;}
+
+        }
+
+        private void TmrShip_Tick(object sender, EventArgs e)
+        {
+            if (left) // if left arrow pressed
+            {
+                if (areaSpaceship.X < 10) //check to see if spaceship within 10 of left side
+                {
+                    areaSpaceship.X = 10; //if it is < 10 away "bounce" it (set position at 10)
+                }
+                else
+                {
+                    areaSpaceship.X -= 5; //else move 5 to the left
+                }
+            }
+
+            if (right) // if right arrow key pressed
+            {
+                if (areaSpaceship.X > PnlGame.Width - 40)// is spaceship within 40 of right side
+                {
+                    areaSpaceship.X = PnlGame.Width - 40;
+                }
+                else
+                {
+                    areaSpaceship.X += 5;
+                }
+            }
+
+            if (up) // if up arrow pressed
+            {
+                if (areaSpaceship.Y < 10) //check to see if spaceship within 10 of left side
+                {
+                    areaSpaceship.Y = 10; //if it is < 10 away "bounce" it (set position at 10)
+                }
+                else
+                {
+                    areaSpaceship.Y -= 5; //else move 5 to the left
+                }
+            }
+
+                if (down) //if down arrow pressed
+                {
+                    if (areaSpaceship.Y < 10) //check to see if spaceship within 10 of left side
+                    {
+                        areaSpaceship.Y = 10; //if it is < 10 away "bounce" it (set position at 10)
+                    }
+                    else
+                    {
+                        areaSpaceship.Y += 10; //else move 5 to the left
+                    }
+                }
+
+
+
+            
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Image[] images = new Image[10];
+        int count;
+        Image planet1;
+
+
+        private void MnuStart_Click(object sender, EventArgs e)
+        {
+            score = 0; //when game starts set the score to 0
+            LblScore1.Text = score.ToString(); //display the score on the form
+            TmrPlanet.Enabled = true; //start the timer to move the planets
+            TmrShip.Enabled = true; //start the timer to move the spaceship
+            TmrAnim.Enabled = true;//start the timer to animate the planets
+
+        }
+
+        private void MnuStop_Click(object sender, EventArgs e)
+        {
+            TmrShip.Enabled = false;
+            TmrPlanet.Enabled = false;
+
+        }
+
+        private void TxtName_TextChanged(object sender, EventArgs e)
+        {
+            string context = TxtName.Text;
+            bool isletter = true;
+            //for loop checks for letters as characters are entered
+            for (int i = 0; i < context.Length; i++)
+            {
+                if (!char.IsLetter(context[i]))// if current character not a letter
+                {
+                    isletter = false;//make isletter false
+                    break; // exit the for loop
+                }
+
+            }
+
+            // if not a letter clear the textbox and focus on it
+            // to enter name again
+            if (isletter == false)
+            {
+                TxtName.Clear();
+                TxtName.Focus();
+            }
+            else
+            {
+                MnuStart.Enabled = true;
+            }
+
+        }
+
+        private void TmrAnim_Tick(object sender, EventArgs e)
+        {
+            //we cycle through each element of the images array creating the animation
+            planet1 = images[count];
+            count++;
+            if (count > 9)
+                count = 1;
+
+            Invalidate();   //refreshes screen otherwise image won't change
+
+        }
+
+        private void LblScore1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtLives_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TmrAnim1_Tick(object sender, EventArgs e)
+        {
+            //we cycle through each element of the images array creating the animation
+            alien1 = images[count];
+            count++;
+            if (count > 9)
+                count = 1;
+
+            Invalidate();   //refreshes screen otherwise image won't change
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public FrmDodge()
+        {
+            InitializeComponent();
+            areaSpaceship = new Rectangle(x2, y2, 35, 30);//spaceship's rectangle	
+            //position the planets
+            for (int i = 0; i < 7; i++)
+            {
+                area[i] = new Rectangle(x + 70 * i, y, 30, 30);
+                planetSpeed[i] = speed.Next(5, 10); //each planet has a random speed
+            }
+        }
+
+        //the CheckLives method will stop the planets and spaceship moving if there are no lives left
+        // and a game over message will be displayed  
+        private void CheckLives()
+        {
+            if (lives == 0)
+            {
+                TmrPlanet.Enabled = false;
+                TmrShip.Enabled = false;
+                MessageBox.Show("Game Over");
+
+            }
+        }
+
+        private void FrmDodge_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= 9; i++)
+            {
+                images[i] = Image.FromFile(Application.StartupPath + @"\planet" + i.ToString() + ".gif");
+            }
+            planet1 = images[1];
+
+            MessageBox.Show("Use the left and right arrow keys to move the spaceship. \n Don't get hit by the planets! \n Every planet that goes past scores a point. \n If a planet hits a spaceship a life is lost!", "Game Instructions");
+            TxtName.Focus();
+            MnuStart.Enabled = false;
+        }
+    }
+}
+
+
+
